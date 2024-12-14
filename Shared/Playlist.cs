e505ed2daf12dfed856c416a988f74588a34e1ad
Shahria Abeed <shahriaabeed@gmail.com>
2024-12-14 13:06:31 -0500

    SHARED - created Playlist object

---
+++ Shared/Playlist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music_manager_starter.Shared
+{
+    public sealed class Playlist
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public int SongCount { get; set; }
+    }
+}
