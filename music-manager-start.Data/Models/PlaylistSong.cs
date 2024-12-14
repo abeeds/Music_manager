6ce7a9d196dbc37f8b60144b6f55c999882943ea
Shahria Abeed <shahriaabeed@gmail.com>
2024-12-14 13:40:54 -0500

    Data - added relationships to PlaylistSong model

--- music-manager-start.Data/Models/PlaylistSong.cs
+++ music-manager-start.Data/Models/PlaylistSong.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using music_manager_starter.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,9 @@ namespace music_manager_start.Data.Models
     {
         public Guid PlaylistId { get; set; }
         public Guid SongId { get; set; }
+
+        // relationships
+        public Playlist Playlist { get; set; }
+        public Song Song { get; set; }
     }
 }
