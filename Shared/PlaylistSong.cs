c829066fa1ab5ba911d8368ea5abdf1b0172057b
Shahria Abeed <shahriaabeed@gmail.com>
2024-12-14 13:37:59 -0500

    DATA, SHARED - added playlist song object and model

---
+++ Shared/PlaylistSong.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music_manager_starter.Shared
+{
+    public sealed class PlaylistSong
+    {
+        public Guid PlaylistId { get; set; }
+        public Guid SongId { get; set; }
+    }
+}
