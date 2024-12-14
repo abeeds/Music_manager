5005e7ed50a3bb11c19385e88a8ca85bf9b7d021
Shahria Abeed <shahriaabeed@gmail.com>
2024-12-14 12:59:15 -0500

    DATA - created Playlist model

---
+++ music-manager-start.Data/Models/Playlist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music_manager_start.Data.Models
+{
+    internal class Playlist
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public int SongCount { get; set; }
+    }
+}
