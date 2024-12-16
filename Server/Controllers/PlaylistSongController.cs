0e0f7c242978168c8eface1a3756cd0882997ade
Shahria Abeed <shahriaabeed@gmail.com>
2024-12-15 22:30:09 -0500

    Server - fixed up Post PlaylistSong endpoint

--- Server/Controllers/PlaylistSongController.cs
+++ Server/Controllers/PlaylistSongController.cs
@@ -12,34 +12,80 @@ namespace music_manager_starter.Server.Controllers
     {
         private readonly DataDbContext _context;
 
-        public PlaylistSongController(DataDbContext context)
+        public PlaylistSongsController(DataDbContext context)
         {
             _context = context;
         }
 
         // returns songs from the specified playlist
+        // also returns that playlists data
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Song>>> GetPlaylistSongs(Guid PlaylistId = null)
+        public async Task<ActionResult<IEnumerable<Song>>> GetPlaylistSongs(Guid PlaylistId)
         {
             if(PlaylistId == Guid.Empty)
                 return BadRequest("PlaylistId cannot be null.");
-            var pls = await _context.PlaylistSongs
-                .Where(pls => pls.PlaylistId == PlaylistId)
+
+            // ensure playlist exists
+            var playlist = await _context.Playlists
+                .Where(pl => pl.Id == PlaylistId)
+                .ToListAsync();
+            if(playlist.Count == 0)
+                return NotFound("Playlist not found.");
+
+            // get all songs in the playlist
+            var plsongs = await _context.PlaylistSongs
+                .Where(ps => ps.PlaylistId == PlaylistId)
+                .GroupJoin(
+                    _context.Songs,
+                    playlistSong => playlistSong.SongId,
+                    song => song.Id,
+                    (playlistSong, songs) => new
+                    {
+                        PlaylistSong = playlistSong,
+                        Song = songs.FirstOrDefault()
+                    }
+                )
                 .ToListAsync();
 
-            if (pls.Count == 0)
+            if (plsongs.Count == 0)
                 return NotFound("No songs are in the specified playlist.");
 
-            return Ok(pls);
+            return Ok(plsongs);
         }
 
         [HttpPost]
-        public async Task<ActionResult<Song>> PostPlaylistSong(Song song)
+        public async Task<ActionResult<Song>> PostPlaylistSong(PlaylistSong PlaylistSong)
         {
-            if (song == null)
-                return BadRequest("Song cannot be null.");
+            // ensure fields are correct
+            if (PlaylistSong == null
+                || PlaylistSong.PlaylistId == Guid.Empty
+                || PlaylistSong.SongId == Guid.Empty
+            )
+                return BadRequest("PlaylistId and SongId cannot be null.");
 
-            _context.PlaylistSongs.Add(song);
+            // ensure playlist exists
+            var playlist = await _context.Playlists
+                .Where(pl => pl.Id == PlaylistSong.PlaylistId)
+                .ToListAsync();
+            if(playlist.Count == 0)
+                return NotFound("Playlist not found.");
+
+            // ensure song exists
+            var s = await _context.Songs
+                .Where(s => s.Id == PlaylistSong.SongId)
+                .ToListAsync();
+            if (s.Count == 0)
+                return NotFound("Song not found.");
+
+            // check if song is already in playlist
+            var pls = await _context.PlaylistSongs
+                .Where(pls => pls.PlaylistId == PlaylistSong.PlaylistId 
+                              && pls.SongId == PlaylistSong.SongId)
+                .ToListAsync();
+            if(pls.Count != 0)
+                return Conflict("Song is already in playlist.");
+
+            _context.PlaylistSongs.Add(PlaylistSong);
             await _context.SaveChangesAsync();
             return Ok();
         }
